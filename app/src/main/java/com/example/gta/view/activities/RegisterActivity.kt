5424c8e6a7ae020b976bd5fba5e0fb562a64a597
Haneol <haneolj@gmail.com>
2024-12-04 20:48:59 +0900

    [feature/login] feat: 회원가입 페이지 UI 구현

--- app/src/main/java/com/example/gta/view/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/gta/view/activities/RegisterActivity.kt
@@ -1,30 +1,21 @@
 package com.example.gta.view.activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gta.R
 
-class IntroActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // 회원가입 버튼
-        val buttonRegister = findViewById<TextView>(R.id.tvRegister)
-        buttonRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
         }
     }
 }
