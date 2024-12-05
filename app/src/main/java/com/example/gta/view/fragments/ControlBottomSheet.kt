7449ef6ab179e2b5f6edd7043da790eae653d045
Haneol <haneolj@gmail.com>
2024-12-06 00:25:42 +0900

    [feature/home] feat: 온도 seekBar 구현

--- app/src/main/java/com/example/gta/view/fragments/ControlBottomSheet.kt
+++ app/src/main/java/com/example/gta/view/fragments/ControlBottomSheet.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import com.example.gta.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -18,7 +20,20 @@ class ControlBottomSheet : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val seekBar = view.findViewById<SeekBar>(R.id.temperatureSeekBar)
+        val tempText = view.findViewById<TextView>(R.id.textViewTemp)
 
+        seekBar.max = 32
+        seekBar.min = 16
+        seekBar.progress = 24
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                tempText.text = "${progress}°C"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 
     companion object {
